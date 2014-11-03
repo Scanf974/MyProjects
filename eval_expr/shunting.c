4041cbd29ffe68d12548a2cd96a9ac421994feef
Scanf974 <sautron.brunojj@gmail.com>
2014-11-03 20:06:46 +0100

    jai le reverse polish enfin :)

--- eval_expr/shunting.c
+++ eval_expr/shunting.c
@@ -32,14 +32,12 @@ void	shunting_yard(t_list **line)
 	stack = init_list(stack);
 	while (temp_line)
 	{
-		ft_putstr("\nla on entre ");
 		if (temp_line->token >= '0' && temp_line->token <= '9')
 			ft_list_push_front(&output_queue, temp_line->token);
 		else if (is_ope(temp_line->token))
 		{
 			if (stack)
 			{
-				ft_putstr("Ya un ope\n");
 				while (stack && is_ope(temp_line->token) <= is_ope(stack->token))
 				{
 					ft_list_push_front(&output_queue, stack->token);
@@ -48,17 +46,30 @@ void	shunting_yard(t_list **line)
 			}
 			ft_list_push_front(&stack, temp_line->token);
 		}
-		else
-		{
-			ft_putstr("mais que cest il passer\n");
+		else if (temp_line->token == '(')
 			ft_list_push_front(&stack, temp_line->token);
+		else if (temp_line->token == ')')
+		{
+			while (stack->token != '(')
+			{
+				ft_list_push_front(&output_queue, stack->token);
+				ft_remove_top(&stack);
+			}
+			ft_remove_top(&stack);
 		}
 
-		ft_putstr("STA ");
+		ft_putstr("\nSTA ");
 		ft_print_list(&stack);
 		ft_putstr("\nOUT ");
 		ft_print_list(&output_queue);
 		ft_putchar('\n');
 		temp_line = temp_line->next;
 	}
+	while (stack)
+	{
+		ft_list_push_front(&output_queue, stack->token);
+		ft_remove_top(&stack);
+	}
+	ft_putstr("et mtn the end is OUT: ");
+	ft_print_list(&output_queue);
 }
