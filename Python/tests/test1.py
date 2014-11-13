f64dfa635b35ef92ba7d32dc2c479be6b251ae5c
Scanf974 <sautron.brunojj@gmail.com>
2014-11-13 12:54:34 +0100

    vrai traj

--- Python/tests/test1.py
+++ Python/tests/test1.py
@@ -1,29 +1,51 @@
 # -*- coding: utf-8 -*-
 
+
 from Tkinter import *
 from math import *
 from scipy import *
 import ImageTk
+import threading
 
-class   First_cls(object):
-    """Me premier class"""
+class   MyTimer:
+    def __init__(self, tempo, target, args= [], kwargs={}):
+        self._target = target
+        self._args = args
+        self._kwargs = kwargs
+        self._tempo = tempo
 
-    def __init__(self, arg1, arg2):
-        self.argv1 = arg1
-        self.argv2 = arg2
+    def _run(self):
+        self._timer = threading.Timer(self._tempo, self._run)
+        self._timer.start()
+        self._target(*self._args, **self._kwargs)
 
-    def fnc(self):
-        print self.argv1
-        print self.argv2
+    def start(self):
+        self._timer = threading.Timer(self._tempo, self._run)
+        self._timer.start()
+
+    def stop(self):
+        self._timer.cancel()
 
 
 class   Objet_de_la_vie:
     """Unobjet avec les propriete Newton"""
 
-    def __init__(self, poids, py, px):
+    def __init__(self, poids, py0, px0):
         self.poids = poids
-        self.py = py
-        self.px = px
+        self.py0 = py0
+        self.px0 = px0
+        self.py = py0
+        self.px = px0
+        
+    def shoot(self, vitesse, angle):
+        self.angle = 0.0174*angle
+        self.vitesse = vitesse
+        self.vx = cos(self.angle) * vitesse
+        self.vy = sin(self.angle) * vitesse
+        self.timer = MyTimer(0.01, self.deplace)
+        self.timer.start()
+
+
 
 class   Gland(Objet_de_la_vie):
     """LE gland"""
@@ -31,19 +53,28 @@ class   Gland(Objet_de_la_vie):
     def __init__(self, poids, py, px):
         Objet_de_la_vie.__init__(self, poids, py, px)
         self.image = can.create_image(self.px, self.py, image = gland_image, anchor = NW)
-
 class   Squirrel(Objet_de_la_vie):
     """LE squirrel"""
 
     def __init__(self, poids, py, px):
         Objet_de_la_vie.__init__(self, poids, py, px)
         self.image = can.create_image(self.px, self.py, image = squirrel_image, anchor = NW)
+        self.tps = 0
 
+    def deplace(self):
+        if (self.py > -20 and self.px < 1000 and self.py <= 340):
+            self.px = self.vx*self.tps + self.px0
+            self.py = ((self.poids*9.81)/2)*self.tps*self.tps - self.vy*self.tps + self.py0
+            can.coords(self.image, self.px, self.py)
+            self.tps += 0.14
+        else:
+            self.timer.stop()
 
 fen = Tk()
 fen.title("BallSquirrel")
 frame = Frame(fen)
 frame.pack()
+
 
 can = Canvas(fen, width = 1200, height = 500, bg = 'white')
 can.pack(side = TOP, padx = 0, pady = 0)
@@ -58,5 +89,7 @@ sol = can.create_image(0, 351, image = sol_image, anchor = NW)
 
 g1 = Gland(1, 100, 400)
 sq = Squirrel(1, 300, 50)
+
+sq.shoot(80, 45)
 
 fen.mainloop()
